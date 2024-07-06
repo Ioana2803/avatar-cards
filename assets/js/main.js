36141a0c9e121b137782bfee3b930047956454c8
Adrian-Moldovan <ma.moldovan@gmail.com>
2024-07-06 14:20:28 +0300

    adds player JS object

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+console.log('All set!');
+
+const player = {
+    name: 'Kiki',
+    stats: {
+        hp: {
+            max: 120,
+            current: 33
+        },
+        energy: {
+            max: 90,
+            current: 68
+        }
+    }
+}
+
+
+console.log(player);
